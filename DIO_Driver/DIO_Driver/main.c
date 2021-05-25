3df2104e61486d8aa877a00f7b15e69698b14892
MarkFayez95 <markfayeztalaat@gmail.com>
2021-05-25 21:10:00 +0200

    DIO Driver adjusted and tested no syntax errors detected

--- DIO_Driver/DIO_Driver/main.c
+++ DIO_Driver/DIO_Driver/main.c
@@ -5,14 +5,44 @@
  * Author : Mark Fayez
  */ 
 
-#include "DIO.h"
+#define F_CPU 16000000
+
+#include "LED.h"
+#include "Button.h"
+#include <Util/delay.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	LED_Init(LED0);
+	LED_Init(LED1);
+	LED_Init(LED2);
+	
+	Button_Init(BUTTON0);
+	Button_Init(BUTTON1);
+	Button_Init(BUTTON2);
+	
+	/* Replace with your application code */
+	while (1)
+	{
+		if(Button_GetValue(BUTTON0) == 1)
+		{
+			LED_Toggle(LED0);
+			_delay_ms(500);
+			LED_Toggle(LED0);
+		}
+		else if(Button_GetValue(BUTTON1) == 1)
+		{
+			LED_Toggle(LED1);
+			_delay_ms(500);
+			LED_Toggle(LED1);
+		}
+		else if(Button_GetValue(BUTTON2) == 1)
+		{
+			LED_Toggle(LED2);
+			_delay_ms(500);
+			LED_Toggle(LED2);
+		}
+		
+	}
 }
-

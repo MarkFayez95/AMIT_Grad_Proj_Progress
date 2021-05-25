7f7a2a031bd9cf0e8428dd0de97f2f01f97d01f4
MarkFayez95 <markfayeztalaat@gmail.com>
2021-05-25 20:07:19 +0200

    Start editing on DIO Driver

---
+++ DIO_Driver/DIO_Driver/main.c
@@ -0,0 +1,18 @@
+/*
+ * DIO_Driver.c
+ *
+ * Created: 25/05/2021 7:51:32 PM
+ * Author : Mark Fayez
+ */ 
+
+#include "DIO.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

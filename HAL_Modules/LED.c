b452dc90e929d0e893fe22661ce3b18b4317d2f4
MarkFayez95 <markfayeztalaat@gmail.com>
2021-05-27 21:07:54 +0200

    LED Module modified V2.0.0

--- HAL_Modules/LED.c
+++ HAL_Modules/LED.c
@@ -8,71 +8,20 @@
 #include "LED.h"
 
 
-void LED_Init(uint8 LED)
-{
-	switch(LED)
-	{
-		case LED0:
-			DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_INPUT);
-		break;
-		
-		case LED1:
-			DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_INPUT);
-		break;
-		
-		case LED2:
-			DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_INPUT);
-		break;
-		
-		case ALL_LEDS:
-			DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_INPUT);
-			DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_INPUT);
-			DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_INPUT);
-		break;
-		
-		default:
-		break;
-	}
-	
-}
 #ifdef CALLBACK_INT_ENABLE
 	void LED0_Init(void)
 	{
-		DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_INPUT);
+		DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_OUTPUT);
 	}
 	void LED1_Init(void)
 	{
-		DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_INPUT);
+		DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_OUTPUT);
 	}
 	void LED2_Init(void)
 	{
-		DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_INPUT);
+		DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_OUTPUT);
 	}
-#endif
 
-#ifndef CALLBACK_INT_ENABLE
-	void LED_ON(uint8 LED)
-	{
-		switch(LED)
-		{
-			case LED0:
-			DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_HIGH);
-			break;
-			
-			case LED1:
-			DIO_SetPinValue(LED1_PORT, LED1_PIN, DIO_PIN_HIGH);
-			break;
-			
-			case LED2:
-			DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_HIGH);
-			break;
-			
-			default:
-			break;
-		}
-	}
-#endif
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_ON(void)
 	{
 		DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_HIGH);
@@ -85,31 +34,7 @@ void LED_Init(uint8 LED)
 	{
 		DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_HIGH);
 	}
-#endif
 
-#ifndef CALLBACK_INT_ENABLE
-	void LED_OFF(uint8 LED)
-	{
-		switch(LED)
-		{
-			case LED0:
-			DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_LOW);
-			break;
-			
-			case LED1:
-			DIO_SetPinValue(LED1_PORT, LED1_PIN, DIO_PIN_LOW);
-			break;
-			
-			case LED2:
-			DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_LOW);
-			break;
-			
-			default:
-			break;
-		}
-	}
-#endif
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_OFF(void)
 	{
 		DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_LOW);
@@ -122,31 +47,7 @@ void LED_Init(uint8 LED)
 	{
 		DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_LOW);
 	}
-#endif
 
-#ifndef CALLBACK_INT_ENABLE
-	void LED_Toggle(uint8 LED)
-	{
-		switch(LED)
-		{
-			case LED0:
-			DIO_TogglePin(LED0_PORT, LED0_PIN);
-			break;
-			
-			case LED1:
-			DIO_TogglePin(LED1_PORT, LED1_PIN);
-			break;
-			
-			case LED2:
-			DIO_TogglePin(LED2_PORT, LED2_PIN);
-			break;
-			
-			default:
-			break;
-		}
-	}
-#endif
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_Toggle(void)
 	{
 		DIO_TogglePin(LED0_PORT, LED0_PIN);
@@ -159,4 +60,96 @@ void LED_Init(uint8 LED)
 	{
 		DIO_TogglePin(LED2_PORT, LED2_PIN);
 	}
+
 #endif
+
+void LED_Init(uint8 LED)
+{
+	switch(LED)
+	{
+		case LED0:
+			DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_OUTPUT);
+		break;
+		
+		case LED1:
+			DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_OUTPUT);
+		break;
+		
+		case LED2:
+			DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_OUTPUT);
+		break;
+		
+		case ALL_LEDS:
+			DIO_SetPinDir(LED0_PORT, LED0_PIN, DIO_PIN_OUTPUT);
+			DIO_SetPinDir(LED1_PORT, LED1_PIN, DIO_PIN_OUTPUT);
+			DIO_SetPinDir(LED2_PORT, LED2_PIN, DIO_PIN_OUTPUT);
+		break;
+		
+		default:
+		break;
+	}
+
+}
+void LED_ON(uint8 LED)
+{
+	switch(LED)
+	{
+		case LED0:
+		DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_HIGH);
+		break;
+		
+		case LED1:
+		DIO_SetPinValue(LED1_PORT, LED1_PIN, DIO_PIN_HIGH);
+		break;
+		
+		case LED2:
+		DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_HIGH);
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void LED_OFF(uint8 LED)
+{
+	switch(LED)
+	{
+		case LED0:
+		DIO_SetPinValue(LED0_PORT, LED0_PIN, DIO_PIN_LOW);
+		break;
+		
+		case LED1:
+		DIO_SetPinValue(LED1_PORT, LED1_PIN, DIO_PIN_LOW);
+		break;
+		
+		case LED2:
+		DIO_SetPinValue(LED2_PORT, LED2_PIN, DIO_PIN_LOW);
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void LED_Toggle(uint8 LED)
+{
+	switch(LED)
+	{
+		case LED0:
+		DIO_TogglePin(LED0_PORT, LED0_PIN);
+		break;
+		
+		case LED1:
+		DIO_TogglePin(LED1_PORT, LED1_PIN);
+		break;
+		
+		case LED2:
+		DIO_TogglePin(LED2_PORT, LED2_PIN);
+		break;
+		
+		default:
+		break;
+	}
+}
+
--- HAL_Modules/LED.h
+++ HAL_Modules/LED.h
@@ -9,51 +9,46 @@
 #ifndef LED_H_
 #define LED_H_
 
+#include "DIO.h"
+
 #define LED0		0
 #define LED1		1
 #define LED2		2
 #define ALL_LEDS	3
 
-#include "DIO.h"
+#define LED0_PORT 	DIO_PORTC
+#define LED0_PIN 	DIO_PIN_2
 
-#define LED0_PORT DIO_PORTC
-#define LED0_PIN DIO_PIN_2
+#define LED1_PORT 	DIO_PORTC
+#define LED1_PIN	DIO_PIN_7
 
-#define LED1_PORT DIO_PORTC
-#define LED1_PIN DIO_PIN_7
+#define LED2_PORT 	DIO_PORTD
+#define LED2_PIN	DIO_PIN_3
 
-#define LED2_PORT DIO_PORTD
-#define LED2_PIN DIO_PIN_3
+//#define CALLBACK_INT_ENABLE	1
 
-#define CALLBACK_INT_ENABLE	1
-
-void LED_Init(uint8 LED);
 #ifdef CALLBACK_INT_ENABLE
 	void LED0_Init(void);
 	void LED1_Init(void);
 	void LED2_Init(void);
-#endif
 
-
-void LED_ON(uint8 LED);
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_ON(void);
 	void LED1_ON(void);
 	void LED2_ON(void);
-#endif
 
-void LED_OFF(uint8 LED);
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_OFF(void);
 	void LED1_OFF(void);
 	void LED2_OFF(void);
-#endif
 
-void LED_Toggle(uint8 LED);
-#ifdef CALLBACK_INT_ENABLE
 	void LED0_Toggle(void);
 	void LED1_Toggle(void);
 	void LED2_Toggle(void);
 #endif
+
+void LED_Init(uint8 LED);
+void LED_ON(uint8 LED);
+void LED_OFF(uint8 LED);
+void LED_Toggle(uint8 LED);
+
 
 #endif /* LED_H_ */
